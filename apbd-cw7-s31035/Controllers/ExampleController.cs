71f102df896d74eb76ac65b30d57fec83f46b842
Mateusz Słomka <slomkam7@gmail.com>
2025-05-03 19:08:47 +0200

    prepare minimal app template

---
+++ apbd-cw7-s31035/Controllers/ExampleController.cs
@@ -0,0 +1,11 @@
+﻿using apbd_cw7_s31035.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apbd_cw7_s31035.Controllers;
+
+[ApiController]
+[Route("/whatever")]
+public class ExampleController(IDbService dbService) : ControllerBase
+{
+
+}
--- apbd-cw7-s31035/Models/Country.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace apbd_cw7_s31035.Models;
-
-public class Country
-{
-    public required int IdCountry { get; set; }
-    public required string Name { get; set; }
-}
